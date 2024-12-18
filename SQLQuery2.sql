e3010917bfd1f8f3665a90fa1f3613c40da6cf10
Shaktiya <shaktiya1111@gmail.com>
2024-12-18 16:07:38 +0530

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE PortfolioEntries (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(100),
+    Email NVARCHAR(100),
+    Phone NVARCHAR(50),
+    Summary NVARCHAR(MAX),
+    WorkExperience NVARCHAR(MAX),
+    Education NVARCHAR(MAX),
+    Skills NVARCHAR(MAX),
+    Certifications NVARCHAR(MAX),
+    Projects NVARCHAR(MAX)
+);
